81bd1be428c5555791c69f9bd7b60497043dd28d
Ru5C55an <ruslan.sn@icloud.com>
2021-08-09 23:28:48 +0300

    Initial Commit

---
+++ vkgallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkgallery
+//
+//  Created by Руслан Садыков on 09.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
