f23b40184ec322dd89706659f971e86ad4018bca
Ru5C55an <ruslan.sn@icloud.com>
2021-08-13 23:51:58 +0300

    Fixed loading indicator start frame for good looks like when image sets from cache

--- vkgallery/Screens/Feed/Views/PhotoLoadingIndicator.swift
+++ vkgallery/Screens/Feed/Views/PhotoLoadingIndicator.swift
@@ -9,8 +9,8 @@ import Kingfisher
 import Lottie
 
 enum ProgressKeyFrames: CGFloat {
-  case start = 0
-  case end = 55
+    case start = 0
+    case end = 55
 }
 
 struct ImageIndicator: Indicator {
@@ -25,31 +25,19 @@ struct ImageIndicator: Indicator {
     }
     
     private func progress(to progress: CGFloat) {
-
-      // 1. We get the range of frames specific for the progress from 0-100%
-      
-      let progressRange = ProgressKeyFrames.end.rawValue - ProgressKeyFrames.start.rawValue
-      
-      // 2. Then, we get the exact frame for the current progress
-      
-      let progressFrame = progressRange * progress
-      
-      // 3. Then we add the start frame to the progress frame
-      // Considering the example that we start in 140, and we moved 30 frames in the progress, we should show frame 170 (140+30)
-      
-      let currentFrame = progressFrame + ProgressKeyFrames.start.rawValue
-      
-      // 4. Manually setting the current animation frame
-      
+        let progressRange = ProgressKeyFrames.end.rawValue - ProgressKeyFrames.start.rawValue
+        let progressFrame = progressRange * progress
+        let currentFrame = progressFrame + ProgressKeyFrames.start.rawValue
         animationView.currentProgress = currentFrame
     }
     
     func startAnimatingView() {
         view.isHidden = false
         animationView.animationSpeed = 0.5
+        animationView.currentFrame = ProgressKeyFrames.end.rawValue
         animationView.play(fromFrame: ProgressKeyFrames.start.rawValue, toFrame: ProgressKeyFrames.end.rawValue, loopMode: .none)
-
     }
+    
     func stopAnimatingView() {
         animationView.stop()
         view.isHidden = true
